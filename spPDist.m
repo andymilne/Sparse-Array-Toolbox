53ed7b1038a74a4564ff877e0f8672e111ab7239
Andrew Milne <andymilne@tonalcentre.org>
2018-01-11 10:42:04 +1100

    spPDist

--- spPDist.m
+++ spPDist.m
@@ -2,9 +2,10 @@ function d = spPDist(spA,spB,p)
 %PDIST p-norm distance between two sparse array structures.
 %   d = spPDist(spA,spB,p): The p-norm distance between two vectorized full
 %   arrays, represented as sparse array structures, with same numbers of
-%   entries. When there are only two arguments, p is taken to be 2, which gives
-%   the Euclidean distance between the two vectors; when p = 1, this function
-%   gives the taxicab distance; when p = inf, it gives the maximum difference.
+%   entries. When there are only two arguments, p = 2 is the default, which
+%   gives the Euclidean distance between the two vectors; when p = 1, this
+%   function gives the taxicab distance; when p = inf, it gives the maximum
+%   difference.
 %
 %   Version 1.0 by Andrew J. Milne, The MARCS Institute, Western Sydney
 %   University, 2018-01-09
