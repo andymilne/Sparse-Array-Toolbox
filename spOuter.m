df9ba86758d8d21cd5845a62c5955e82f0286b48
Andrew Milne <andymilne@tonalcentre.org>
2018-01-13 19:14:29 +1100

    Tidy up loop and preallocate sizeCell (formerly sizeArray)

--- spOuter.m
+++ spOuter.m
@@ -1,12 +1,12 @@
 function spC = spOuter(varargin)
 %SPOUTER THE Outer (tensor) product of sparse array structures.
 %   spC = spOuter(varargin): The outer (tensor) product of full arrays, each
-%   represented as a sparse array structure. The sparse array structures can
+%   represented as a sparse array structure. The sparse array structures can be
 %   entered as a comma separated list or as a members of a cell. The output is
 %   a sparse array structure. Calculations are performed from left to right in
-%   the list. All singleton dimensions are collapsed: if A is a row vector of
+%   the list. All singleton dimensions are removed: if A is a row vector of
 %   size (1,M) and B is a matrix of size (N,P), the resulting tensor has size
-%   (M, N, P), not size (1,M,N,P). The output is a sparse array structure.
+%   (M,N,P), not size (1,M,N,P). The output is a sparse array structure.
 %
 %   Version 1.0 by Andrew J. Milne, The MARCS Institute, Western Sydney
 %   University, 2018-01-09
@@ -19,37 +19,34 @@ elseif iscell(varargin{1})
 end
 nSpA = size(spA,2); % count the number of arguments
 
-valspA = 1;
-indspA = spA{1}.Ind;
 numPrev = 1;
-sizeArray = [];
-for i = 1:nSpA
-    % Calculate the values
-    valTermi = spA{i}.Val;
-    if i > 1
-        % Orthogonalize vector representations of sparse arrays' values to make
-        % use of implicit expansion
-        valTermi = permute(valTermi,circshift(1:nSpA,i-1));
-    end
+valspA = spA{1}.Val;
+indspA = spA{1}.Ind;
+sizeCell = cell(1,nSpA);
+sizeCell{1} = spA{1}.Size;
+for i = 2:nSpA
+    % Calculate values:
+    % Orthogonalize vector representations of sparse arrays' values to make use
+    % of implicit expansion
+    valTermi = permute(spA{i}.Val,circshift(1:nSpA,i-1));
     % Outer product of nonzeros in original arrays
-    valspA = valspA.*valTermi; 
+    valspA = valspA.*valTermi;
     
-    % Calculate the respective indices
-    if i > 1
-        % Linear indices of nonzeros in ith array
-        indTermi = spA{i}.Ind;
-        % Orthogonalize vector representations of sparse arrays' indices to
-        % make use of implicit expansion
-        indTermi = permute(indTermi,circshift(1:nSpA,i-1));
-        % Get size of previous dimension of original array
-        numPrevi = prod(spA{i-1}.Size);
-        % Calculate the linear index weights for each successive dimension
-        numPrev = numPrev*numPrevi;
-        % Weighted outer sum of indices of nonzero entries in arrays
-        indspA = indspA + numPrev*(indTermi-1); 
-    end
-    sizeArray = [sizeArray spA{i}.Size]; 
+    % Calculate linear indices:
+    % Orthogonalize vector representations of sparse arrays' indices to
+    % make use of implicit expansion
+    indTermi = permute(spA{i}.Ind,circshift(1:nSpA,i-1));
+    % Get size of previous dimension of original array
+    numPrevi = prod(spA{i-1}.Size);
+    % Calculate the linear index weights for each successive dimension
+    numPrev = numPrev*numPrevi;
+    % Weighted outer sum of indices of nonzero entries in arrays
+    indspA = indspA + numPrev*(indTermi-1);
+    sizeCell{i} = spA{i}.Size;
 end
+sizeArray = cat(2,sizeCell{:});
+
+% Make the sparse array structure
 spC = struct('Size',sizeArray,'Ind',indspA(:),'Val',valspA(:));
 
 end
