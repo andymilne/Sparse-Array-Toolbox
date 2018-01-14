518bfc67f0ff4d1bc91055e0428b51727fdf079b
Andrew Milne <andymilne@tonalcentre.org>
2018-01-14 15:26:25 +1100

    Change variable name

--- spCosSim.m
+++ spCosSim.m
@@ -2,13 +2,14 @@ function s = spCosSim(spA,spB)
 %SPCOSSIM Cosine similarity of two sparse array structures
 %   s = spCosSim(spA,spB):  Cosine similarity of two vectorized full arrays,
 %   represented by sparse array structures, with the same numbers of entries.
+%   The output is a scalar.
 %
 %   Version 1.0 by Andrew J. Milne, The MARCS Institute, Western Sydney
 %   University, 2018-01-09
 %
 %   See also SPPDIST.
 
-persistent ipAA ipBB spALast spBLast
+persistent innerAA innerBB spALast spBLast
 
 if ~isstruct(spA) || ~isstruct(spB)
     error('Arguments must be (a cell array of) sparse array structures.')
@@ -21,12 +22,12 @@ end
 spANew = ~isequal(spA,spALast);
 spBNew = ~isequal(spB,spBLast);
 if spANew
-    ipAA = spInner(spA,spA);
+    innerAA = spInner(spA,spA);
 end
 if spBNew
-    ipBB = spInner(spB,spB);
+    innerBB = spInner(spB,spB);
 end
-s = spInner(spA,spB)/sqrt(ipAA*ipBB);
+s = spInner(spA,spB)/sqrt(innerAA*innerBB);
 
 spALast = spA;
 spBLast = spB;
