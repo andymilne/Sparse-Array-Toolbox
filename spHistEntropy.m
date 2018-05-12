3cbab8badf3ff8ab7d989e748b7ee0dc51c35cf6
Andrew Milne <andymilne@tonalcentre.org>
2018-05-12 17:56:11 +1000

    Initial commit

---
+++ spHistEntropy.m
@@ -0,0 +1,24 @@
+function h = spHistEntropy(spA)
+%SPHISTENTROPY Normalized entropy of a histogram in sparse array format.
+%
+%   h = spHistEntropy(v): Normalized entropy of a histogram v, where the
+%   histogram is normalized to convert it into a probability mass function so
+%   its emtropy can be calculated. Normalized entropy is entropy divided by
+%   maximum possible entropy; it is, therefore, unitless and in the interval
+%   [0,1]; it allows for comparisons between differently sized sample spaces.
+%
+%   By Andrew J. Milne, The MARCS Institute, Western Sydney University.
+
+
+% Normalize to make into probability mass function
+histNorm = spA.Val./sum(spA.Val);
+
+% Size of sample space
+N = prod(spA.Size);
+
+% Calculate entropy
+logHistNorm = -log(histNorm);
+logHistNorm(isinf(logHistNorm)) = 0;
+h = histNorm'*logHistNorm/log(N);
+
+end
