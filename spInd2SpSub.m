5897b29d7546f8bc5dea429d520505ce78a35d32
Andrew Milne <andymilne@tonalcentre.org>
2025-01-25 14:09:03 +1100

    Make compatible with 1D input

--- spInd2SpSub.m
+++ spInd2SpSub.m
@@ -1,17 +1,21 @@
 function subsA = spInd2SpSub(spA)
-%SPIND2SPSUB Convert a sparse array's linear index into a matrix of subscripts. 
+%SPIND2SPSUB Convert a sparse array's linear index into a matrix of subscripts.
 %
 %   subsA = spInd2spSub(spA)
 %
 %   By Andrew J. Milne, The MARCS Institute, Western Sydney University
-%   
+%
 %   See also spSub2SpInd, spSub4Sp, ind2sub, sub2ind.
 
 % Convert spA's linear index to subscripts
 nDimA = numel(spA.Size);
-cellSubA = cell(1,nDimA); % create empty cell for next line
-[cellSubA{:}] = ind2sub(spA.Size,spA.Ind);
-subsA = [cellSubA{:}];
+if nDimA == 1
+    subsA = spA.Ind;
+else
+    cellSubA = cell(1,nDimA); % create empty cell for next line
+    [cellSubA{:}] = ind2sub(spA.Size,spA.Ind);
+    subsA = [cellSubA{:}];
+end
 
 end
 
